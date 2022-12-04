ed54f65e115c2f7d9a5e0054e8c840d3a53bae11
Indivicivet <indivicivet@gmail.com>
2022-12-04 00:17:06 +0000

    add script to move backpack item

---
+++ day03/BackpackItem.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+var v = 50
+var right_side = false
+
+#func _ready():
+	#pass
+
+func _process(delta):
+	move_and_collide(
+		Vector2(v * delta * (-1 if right_side else 1), 0)
+	)
