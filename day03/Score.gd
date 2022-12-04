f0ac2dd4c2c14f8982fd58e6155f14a2127bcbce
Indivicivet <indivicivet@gmail.com>
2022-12-04 12:53:55 +0000

    actually calculate score based on priority values

--- day03/Score.gd
+++ day03/Score.gd
@@ -8,5 +8,9 @@ func _ready():
 	self.text = "Score: " + str(score)
 
 func _on_items_collided(text):
-	score += 1
+	score += (
+		ord(text) - 97 + 1  # a
+		if ord(text) >= 97
+		else ord(text) - 65 + 27  # A
+	)
 	self.text = "Score: " + str(score)
