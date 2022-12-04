5b5b962e2a396f7a55e1bd7a6580829a69f9053f
Indivicivet <indivicivet@gmail.com>
2022-12-04 13:52:26 +0000

    try a better version of exploding

--- day03/Explosion.gd
+++ day03/Explosion.gd
@@ -1,16 +1,12 @@
 extends Area2D
 
 
-export var text_match = "A"
+var text = "A"
 
 
 func _ready():
-	self.connect("area_entered", self, "_area_entered")
-
-
-func _area_entered(other):
-	if not other is BackpackItem:
-		return
-	if other.text != text_match:
-		return
-	other.queue_free()
+	for other in get_overlapping_areas():
+		var child = other.get_node("RichTextNode")
+		if child and child.text == text:
+			print_debug("exploded")
+			other.queue_free()
