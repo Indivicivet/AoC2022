5d0155ff0dd02e4e07f60e14d63ad055de3c0f13
Indivicivet <indivicivet@gmail.com>
2022-12-04 12:35:33 +0000

    add collision events

--- day03/BackpackItem.gd
+++ day03/BackpackItem.gd
@@ -3,8 +3,11 @@ extends Area2D
 var v = 50
 export var right_side = false
 
-#func _ready():
-	#pass
+func _ready():
+	self.connect("area_entered", self, "_area_entered")
 
 func _process(delta):
 	self.position += Vector2(v * delta * (-1 if right_side else 1), 0)
+
+func _area_entered(other):
+	print_debug(other)
