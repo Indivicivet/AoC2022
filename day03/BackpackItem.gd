2623d44f003d852bf4203289120089de2df64260
Indivicivet <indivicivet@gmail.com>
2022-12-04 12:18:43 +0000

    convert KinematicBody2Ds to Area2Ds

--- day03/BackpackItem.gd
+++ day03/BackpackItem.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Area2D
 
 var v = 50
 export var right_side = false
@@ -7,6 +7,4 @@ export var right_side = false
 	#pass
 
 func _process(delta):
-	move_and_collide(
-		Vector2(v * delta * (-1 if right_side else 1), 0)
-	)
+	self.position += Vector2(v * delta * (-1 if right_side else 1), 0)
