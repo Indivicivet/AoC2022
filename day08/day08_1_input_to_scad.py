d53fbd7a7f889d716fc490f0be941ff382b7900c
indi <indivicivet@gmail.com>
2022-12-09 00:39:18 +0000

    add script to convert day08 input to a scad file

---
+++ day08/day08_1_input_to_scad.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import openpyscad as ops
+
+data = [
+    [int(x) for x in line.strip()]
+    for line in (
+        Path(__file__).parent.parent / "inputs" / "input_day08.txt"
+    ).read_text().splitlines()
+]
+
+openscad_obj = ops.Union()
+for j in range(len(data)):
+    for i in range(len(data[0])):
+        openscad_obj.append(
+            ops.Cube([0.8, 0.8, data[j][i]]).translate([i, j, 0])
+        )
+openscad_obj.write("day08.scad")
