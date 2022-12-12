63de53409e4937524af935022d5b8ed6a87960c7
indi <indivicivet@gmail.com>
2022-12-12 00:47:27 +0000

    WIP magnetism code that doesn't work yet

--- day05/Magnet.gd
+++ day05/Magnet.gd
@@ -9,3 +9,18 @@ func _process(delta):
 		position.x += speed * delta;
 	if Input.is_action_pressed("ui_left"):
 		position.x -= speed * delta;
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		var result_raydown = (
+			get_world_2d().direct_space_state.intersect_ray(
+				global_position,
+				global_position + Vector2(0, -800),
+				[self]
+			)
+		)
+		if result_raydown:
+			print("hello")
+			result_raydown.collider.parent.transform.y -= 200 * delta;
+		
