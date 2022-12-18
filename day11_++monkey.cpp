0d823295dad8ef84bbe84153f213385940f39db9
indi <indivicivet@gmail.com>
2022-12-18 21:58:00 +0000

    rename ++monkey mult_by to add_amount, because it is

--- day11_++monkey.cpp
+++ day11_++monkey.cpp
@@ -3,20 +3,20 @@
 class PlusPlusMonkey {
     public:
     std::vector<int> items;
-    int mult_by;
+    int add_amount;
     int divisible_test;
     int true_target;
     int false_target;
 
     PlusPlusMonkey(
         std::vector<int> items,
-        int mult_by,
+        int add_amount,
         int divisible_test,
         int true_target,
         int false_target
     )
     : items(items)
-    , mult_by(mult_by)
+    , add_amount(add_amount)
     , divisible_test(divisible_test)
     , true_target(true_target)
     , false_target(false_target)
