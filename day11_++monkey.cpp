7480f54d40c6f4e69d0335b47c5dd1f9e4ce2cd7
indi <indivicivet@gmail.com>
2022-12-19 16:28:11 +0000

    use uint64_t rather than int, and vector so that can initializer-list construct from python. works!

--- day11_++monkey.cpp
+++ day11_++monkey.cpp
@@ -2,16 +2,16 @@
 
 class PlusPlusMonkey {
     public:
-    std::list<int> items;
-    int add_amount;
+    std::vector<uint64_t> items;
+    uint64_t add_amount;
     int divisible_test;
     int true_target;
     int false_target;
     int inspected = 0;
 
     PlusPlusMonkey(
-        std::list<int> items,
-        int add_amount,
+        std::vector<uint64_t> items,
+        uint64_t add_amount,
         int divisible_test,
         int true_target,
         int false_target
@@ -23,22 +23,22 @@ class PlusPlusMonkey {
     , false_target(false_target)
     {}
 
-    int pop_item() {
+    uint64_t pop_item() {
         const auto x = items.front();
-        items.pop_front();
+        items.erase(items.begin());
         return x;
     }
 
-    void receive(int item) {
+    void receive(uint64_t item) {
         items.push_back(item);
     }
 
-    int next_worry(int worry) {
+    uint64_t next_worry(uint64_t worry) {
         inspected++;
         return worry + add_amount;
     }
 
-    int throw_to(int worry) {
+    uint64_t throw_to(uint64_t worry) {
         return (worry % divisible_test) ? false_target : true_target;
     }
 };
